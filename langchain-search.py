7fd6ccb4ade19b4d1e9570f733167a1cf7a02bb3
Taha Uygun <tahauygun@gmail.com>
2025-12-21 23:26:59 -0800

    fix for the agentexecutor

--- langchain-search.py
+++ langchain-search.py
@@ -3,19 +3,19 @@ load_dotenv()
 
 from langchain_openai import ChatOpenAI
 from langchain_tavily import TavilySearch
-from langchain.agents import AgentExecutor, create_react_agent
+from langchain.agents import create_agent
 from langsmith import Client
 client = Client()
 
 tools = [TavilySearch()]
 llm = ChatOpenAI(model="gpt-4")
-react_prompt = client.pull_prompt("hwchase17/react", include_model=True)
+react_prompt_template = client.pull_prompt("hwchase17/react", include_model=True)
+react_prompt = react_prompt_template.template
 
-agent = create_react_agent(llm=llm, tools=tools, prompt=react_prompt)
-agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
-chain = agent_executor
+agent = create_agent(model=llm, tools=tools, system_prompt=react_prompt)
+
 def main():
-   result = chain.invoke({"input": "Search for engineering manager in AI & ML 3 job posting in Californa using langchain"})
+   result = agent.invoke({"input": "Search for engineering manager in AI & ML 3 job posting in Californa using langchain"})
    print(result)
    
 if __name__ == "__main__":
