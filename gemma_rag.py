35124e0a22f56d66c508edb489c22202c7f76898
Taha Uygun <tahauygun@gmail.com>
2025-12-24 00:18:12 -0800

    adding tokenize and prompt

--- gemma_rag.py
+++ gemma_rag.py
@@ -25,8 +25,33 @@ tokenizer = AutoTokenizer.from_pretrained(
     add_eos_token=True)
 tokenizer.pad_token = tokenizer.eos_token
 
-train_dataset = load_dataset('GEM/viggo', split='train')
-eval_dataset = load_dataset('GEM/viggo', split='validation')
-test_dataset = load_dataset('GEM/viggo', split='test')
+train_dataset = load_dataset('gem/viggo', split='train', trust_remote_code=True)
+eval_dataset = load_dataset('gem/viggo', split='validation', trust_remote_code=True)
+test_dataset = load_dataset('gem/viggo', split='test', trust_remote_code=True)
 
 print(test_dataset)
+
+def tokenize(prompt):
+    result = tokenizer(
+        prompt,
+        truncation=True,
+        max_length=512,
+        padding="max_length",
+    )
+    result["labels"] = result["input_ids"].copy()
+    return result
+
+def generate_and_tokenize_prompt(data_point):
+    full_prompt =f"""Given a target sentence construct the underlying meaning representation of the input sentence as a single function with attributes and attribute values.
+This function should describe the target string accurately and the function must be one of the following ['inform', 'request', 'give_opinion', 'confirm', 'verify_attribute', 'suggest', 'request_explanation', 'recommend', 'request_attribute'].
+The attributes must be one of the following: ['name', 'exp_release_date', 'release_year', 'developer', 'esrb', 'rating', 'genres', 'player_perspective', 'has_multiplayer', 'platforms', 'available_on_steam', 'has_linux_release', 'has_mac_release', 'specifier']
+
+
+### Target sentence:
+{data_point["target"]}
+
+
+### Meaning representation:
+{data_point["meaning_representation"]}
+"""
+    return tokenize(full_prompt)
