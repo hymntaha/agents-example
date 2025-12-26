0dc8a6a40faf6b33e7305ce964de0520d71cfcd2
Taha Uygun <tahauygun@gmail.com>
2025-12-25 23:24:24 -0700

    model eval

--- gemma_rag.py
+++ gemma_rag.py
@@ -62,3 +62,19 @@ print(tokenized_val_dataset)
 
 print("Target Sentence: " + test_dataset[1]['target'])
 print("Meaning Representation: " + test_dataset[1]['meaning_representation'] + "\n")
+
+eval_prompt = """Given a target sentence construct the underlying meaning representation of the input sentence as a single function with attributes and attribute values.
+This function should describe the target string accurately and the function must be one of the following ['inform', 'request', 'give_opinion', 'confirm', 'verify_attribute', 'suggest', 'request_explanation', 'recommend', 'request_attribute'].
+The attributes must be one of the following: ['name', 'exp_release_date', 'release_year', 'developer', 'esrb', 'rating', 'genres', 'player_perspective', 'has_multiplayer', 'platforms', 'available_on_steam', 'has_linux_release', 'has_mac_release', 'specifier']
+
+
+### Target sentence:
+Earlier, you stated that you didn't have strong feelings about PlayStation's Little Big Adventure. Is your opinion true for all games which don't have multiplayer?
+
+
+### Meaning representation:
+"""
+model_input = tokenizer(eval_prompt, return_tensors="pt").to(device)
+model.eval()
+with torch.no_grad():
+    print(tokenizer.decode(model.generate(**model_input, max_new_tokens=256, pad_token_id=2)[0], skip_special_tokens=True))
